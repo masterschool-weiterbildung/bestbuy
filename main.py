e12bea0369535079529af46aecbe4cf537164af7
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-11-09 20:34:57 +0100

    create Product class

---
+++ main.py
@@ -0,0 +1,21 @@
+from products import Product
+
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+    bose.show()
+    mac.show()
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    print(bose.show())
+    print(mac.show())
+
+    bose.set_quantity(1000)
+    print(bose.show())
+
+
+if __name__ == '__main__':
+    main()
---
+++ products.py
@@ -0,0 +1,82 @@
+class Product:
+    def __init__(self, name: str, price: float, quantity: int):
+        self.__name = name  # Private attribute, encapsulation
+        self.__price = price
+        self.__quantity = quantity
+        self.__is_active = True
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        if isinstance(name, str) and name.strip():
+            self.__name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
+
+    def get_price(self):
+        return self.__price
+
+    def set_price(self, price):
+        if isinstance(price, float) and price >= 0.0:
+            self.__price = price
+        else:
+            raise ValueError("Price must be a float")
+
+    def get_quantity(self):
+        return self.__quantity
+
+    def set_quantity(self, quantity):
+        if isinstance(quantity, int) and quantity >= 0:
+            self.__quantity = quantity
+        else:
+            raise ValueError("Quantity must be a int")
+
+        if self.get_quantity() == 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self.__is_active
+
+    def set_is_active(self, is_active):
+        if isinstance(is_active, bool):
+            self.__is_active = is_active
+        else:
+            raise ValueError("Is_active must be a bool")
+
+    def activate(self):
+        self.set_is_active(True)
+
+    def deactivate(self):
+        self.set_is_active(False)
+
+    def __buy_product(self, given_quantity):
+        self.set_quantity(self.get_quantity() - given_quantity)
+
+    def buy(self, quantity):
+        try:
+            # Buys a given quantity of the product.
+            self.__buy_product(quantity)
+        except ValueError as e:
+            print(f"Value Error: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        else:
+            return quantity * self.get_price()
+
+    def show(self):
+        return self.__str__()
+
+    def __str__(self):
+        return (f"{self.get_name()}, Price: {self.get_price()},"
+                f" Quantity: {self.get_quantity()}"
+                f" Is_Active: {self.is_active()}")
+
+    def __eq__(self, other):
+        if isinstance(other, Product):
+            return (self.get_name() == other.get_name()
+                    and self.get_price() == other.get_price()
+                    and self.get_quantity() == other.get_quantity()
+                    and self.is_active() == other.is_active()
+                    )
+        return False
